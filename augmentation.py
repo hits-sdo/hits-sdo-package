a406edb9153ffae8ee4527a4736a8dbd66d65f34
subhamoysgit <sc8473@gmail.com>
2023-03-10 18:23:26 -0700

    team yellow added 'perform_augmentations' method to perform combination of augmentations with a dictionary of methods
    Co-authored-by: Nicholas King <Starfractor@users.noreply.github.com>
    Co-authored-by: Cameron Wolff <camwolff02@users.noreply.github.com>
    Co-authored-by: Sierra Martin <mcdipples@users.noreply.github.com>
    Co-authored-by: Spencer Glenn <Gryuuum@users.noreply.github.com>
    Co-authored-by: theProgrammingBox <theProgrammingBox@users.noreply.github.com>
    Co-authored-by: Ilya Klimov <Ilya2000Klimov@users.noreply.github.com>

--- augmentation.py
+++ augmentation.py
@@ -15,6 +15,12 @@ Plan for Mar 10:
 
 import numpy as np
 import cv2 as cv
+import pickle
+import os
+
+
+
+
 
 
 class Augmentations():
@@ -26,7 +32,7 @@ class Augmentations():
         self 
         dictionary of augmentations
             rotation    (float) : degrees
-            brightness  (float) : 1 = no brightness transformation applied
+            brighten  (float) : 1 = no brighten transformation applied
             zoom        (float) : 1 = 1X zoom
             translate   (integer, integer) : (x,y) || (0,0)  = no translation 
 
@@ -34,11 +40,20 @@ class Augmentations():
     """
 
     # https://www.pythoncheatsheet.org/cheatsheet/dictionaries
-    def __init__(self, image=None, dct={"brightness": 1, "translate": (0,0), "zoom": 1, "rotate": 0, "h_flip": False, "v_flip": False, 'blur':(1,1), "p_flip": False} ):
+    def __init__(self, image=None, dct={"brighten": 1, "translate": (0,0), "zoom": 1, "rotate": 0, "h_flip": False, "v_flip": False, 'blur':(1,1), "p_flip": False} ):
         self.image = image
         self.augmentations = dct
+        #self.augmentationPointer = {"brighten": self.brighten, "translate": self.translate_image, "zoom": self.zoom, 
+       #                        "blur": self.blur_image, "p_flip": self.pole_flip_image}
+        # augmentation_name
+        # augmentationPointer[augmentation_name](image, augmentations[augmentation_name])
+        method_names = [attribute for attribute in dir(self) if callable(getattr(self, attribute)) and attribute.startswith('__') is False]
+        self.augmentationPointer = {}
 
-    def rotate_image(self, image, rotation=0):
+        for name in method_names:
+            self.augmentationPointer[name] = getattr(self, name)
+
+    def rotate(self, image, rotation=0):
         s = image.shape
         cy = (s[0]-1)/2
         cx = (s[1]-1)/2    #set the x, y vals of center of image
@@ -47,47 +62,69 @@ class Augmentations():
         return cv.warpAffine(image,M,(s[1],s[0]))
         
         
-    def brighten_image(self, image, brightness=1):
-        image = image.astype(float)/255
-        image_out = image**brightness
+    def brighten(self, image, brighten=1):
+        #image = image.astype(float)/255
+        image_out = image**brighten
         return image_out
     
 
     # https://docs.opencv.org/4.x/da/d6e/tutorial_py_geometric_transformations.html
-    def translate_image(self, image, translate=(0,0)):
+    def translate(self, image, translate=(0,0)):
         rows,cols = image.shape
         M = np.float32([[1, 0, translate[0]], [0, 1, translate[1]]])
         image = cv.warpAffine(image,M,(cols,rows))
         return image
 
-    def resize(self, image, zoom):
+    def zoom(self, image, zoom):
         dim = image.shape
         return cv.resize(image, (int(zoom*dim[0]), int(zoom*dim[1])), interpolation = cv.INTER_AREA)
         
-    def v_flip_image(self, image, v_flip=True):
+    def v_flip(self, image, v_flip=True):
         if v_flip == True:
             image = cv.flip(image, 1) 
         return image
     
-    def h_flip_image(self, image, h_flip=True):
+    def h_flip(self, image, h_flip=True):
         if h_flip == True:
             image = cv.flip(image, 0) 
         return image
     
-    def blur_image(self, image, blur=(1,1)):
+    def blur(self, image, blur=(1,1)):
         image = cv.blur(image,(blur[0],blur[1]), 0)
         return image
 
-    def pole_flip_image(self, image, p_flip=True):
+    def p_flip(self, image, p_flip=True):
         if p_flip == True:
-            image = 255 - image
+            image = 1 - image
         return image
 
 
     def perform_augmentations(self):
-        ...
-        
+        #*args **kwargs   // pass var num of args to a function
+        augmentations_list = list(self.augmentations.keys())
 
+        augment_image = self.image
+        title = 'original'
+        for augmentation_name in augmentations_list:
+            augment_image = self.augmentationPointer[augmentation_name](
+                augment_image, self.augmentations[augmentation_name]
+            )
+
+            u_arrow = "\u27F6"
+            title = title + u_arrow + augmentation_name
+
+        s = augment_image.shape
+        s1 = (64, 64)  #TODO make flexible
+        y1 = s[0]//2 - s1[0]//2
+        y2 = s[0]//2 + s1[0]//2
+        x1 = s[1]//2 - s1[1]//2
+        x2 = s[1]//2 + s1[1]//2
+        augment_image = augment_image[y1:y2, x1:x2]
+
+    
+        return augment_image,title
+        
+ 
 
 ### json file with a list of aug: object like structure -> image, : List of aougment preformed, and their description
 ### 
