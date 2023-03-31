cac4353740e47dc9901e5f0b0ef14fe05639309e
subhamoysgit <sc8473@gmail.com>
2023-03-30 18:36:10 -0600

    added augmentation_gui.py to create Graphical User Interface for data augmentation

---
+++ augmentation_gui.py
@@ -0,0 +1,17 @@
+'''
+Objective:
+
+
+Plan for today Mar 30, 2023:
+
+'''
+
+import os
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+import cv2 as cv
+import glob
+from augmentation_list import AugmentationList
+from augmentation import Augmentations
+import streamlit as st
