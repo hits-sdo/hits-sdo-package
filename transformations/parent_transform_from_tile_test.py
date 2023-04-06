93ebd71f6638899b31491c4f86a87c7027882f33
Jennifer <jennil38@uci.edu>
2023-04-05 20:06:53 -0700

    Finish calculating total amount of padding needed

--- transformations/parent_transform_from_tile_test.py
+++ transformations/parent_transform_from_tile_test.py
@@ -1,20 +1,20 @@
 import unittest
-import output_tile_info
+import parent_transform_from_tile
 import pyprojroot
 
 
 class Test_Tile_Items(unittest.TestCase):
     def setUp(self) -> None:
         """This class contains the unit tests that the tile object is valid."""
-        self.tile_item = output_tile_info.TileItem( 
-            tile_name="Name", 
-            tile_image_type="jpeg", 
-            tile_is_valid=False,
-            tile_time_stamp="blah",  
-            tile_f_out_path=pyprojroot.here(),  
+        self.tile_item = parent_transform_from_tile.ParentTransformationsFromTile( 
+            # tile_name="Name", 
+            # tile_image_type="jpeg", 
+            # tile_is_valid=False,
+            # tile_time_stamp="blah",  
+            # tile_f_out_path=pyprojroot.here(),  
             tile_pixel_width=3,
             tile_pixel_height=2,
-            is_padded=False,
+            # is_padded=False,
             parent_img_width=27,
             parent_img_height=4,
             parent_file_isValid=True,
