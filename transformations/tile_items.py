98e7f19fdd027a80520a8b5c3dd63728321e9602
nadia ahmed <ahmedn@uci.edu>
2023-04-14 17:21:41 -0700

    started tile_items.py to create a namedtuple of individual tiles

--- transformations/tile_items.py
+++ transformations/tile_items.py
@@ -1,9 +1,39 @@
+import PIL
 from dataclasses import dataclass
+from typing import Typing
+from typing import NamedTuple
 
-@dataclass
 
-class tile_items:
-    """"""
-
+class TileItem(NamedTuple):
     tile_width:int
     tile_height:int
+    origin_row: int
+    origin_col: int
+
+class SeparateClass:
+    """This class divides the parent image into tiles"""
+    parent_image: bytearray
+    #tile_image: 2D array 
+    
+
+    #tile_group: tuple #user can specify which groupings of tiles they want from parent image
+
+    #in this case, tile1 = TileItem(width, )
+    
+    def grab_tiles():
+        ...
+
+
+# creating a class
+# class Website(NamedTuple):
+#     name: str
+#     url: str
+#     rating: int
+  
+# # creating a NamedTuple
+# website1 = Website('GeeksforGeeks',
+#                    'geeksforgeeks.org',
+#                    5)
+  
+# # displaying the NamedTuple
+# print(website1)
